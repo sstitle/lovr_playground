489fd7c98ca880a69716eec780540dcfc0edf55e
Sam Title <samstitle@gmail.com>
2025-02-09 19:38:23 -0500

    Monkey and Cube

--- main.lua
+++ main.lua
@@ -1,3 +1,17 @@
-function lovr.draw(pass)
-    pass:text('hello world', 0, 1.7, -3, .5)
+function lovr.load()
+    -- Load a 3D model
+    model = lovr.graphics.newModel('monkey.obj')
+  
+    -- Use a dark grey background
+    lovr.graphics.setBackgroundColor(.2, .2, .2)
+  end
+  
+  function lovr.draw(pass)
+    -- Draw the model
+    pass:setColor(1, 1, 1)
+    pass:draw(model, -.5, 1, -3)
+  
+    -- Draw a red cube using the "cube" primitive
+    pass:setColor(1, 0, 0)
+    pass:cube(.5, 1, -3, .5, lovr.timer.getTime())
   end
